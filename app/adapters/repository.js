3a73f6c17bc66667960d6b60b500627cf92be48b
jadex1 <greatwolf3d@gmail.com>
2016-06-16 23:14:52 -0700

    Done stuff

---
+++ app/adapters/repository.js
@@ -0,0 +1,12 @@
+import ApplicationAdapter from './application';
+
+export default ApplicationAdapter.extend({
+  headers: {
+    Authorization: "05cb839c1d63c815f3880dbe42f40a74ec020c8b"
+  },
+  updateRecord(store, type, snapshot) {
+    const url = `${this.host}/user/starred/${snapshot.attr('fullName')}`;
+    return this.ajax(url, "PUT", {});
+  }
+
+});
---
+++ app/serializers/application.js
@@ -0,0 +1,9 @@
+// app/serializer/application.js
+import Ember from 'ember';
+import JSONSerializer from 'ember-data/serializers/json';
+
+export default JSONSerializer.extend({
+  keyForAttribute(key){
+    return Ember.String.decamelize(key);
+  }
+});
