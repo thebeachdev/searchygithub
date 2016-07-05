af5ebe2294b1f45cedf481a335fb4d91efc9b789
jadex1 <greatwolf3d@gmail.com>
2016-07-05 10:30:33 -0700

    Working condition

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -9,20 +9,4 @@ export default Ember.Controller.extend({
       this.set('q', this.get('searchValue'));
     }
   }
-
-  // //TODO: need to get this double checked.
-  // queryParams:['search'],
-  // search: null,
-  // //TODO: I want to research this again.
-  // // queryParams:{
-  // //   q: {refreshModel: true}
-  // // },
-  //
-  // searchValue: Ember.computed.oneWay('search'),
-  // actions: {
-  //   submitSearch() {
-  //     this.set('search', this.get('searchValue'));
-  //     this.send('invalidateModel');
-  //   }
-  // }
 });
