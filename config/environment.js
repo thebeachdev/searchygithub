73f1c9180ebdc18471f479449e5ab85e5bf56b12
jadex1 <greatwolf3d@gmail.com>
2016-07-05 17:00:44 -0700

    Added room for api key

--- config/environment.js
+++ config/environment.js
@@ -21,6 +21,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.githubKey = process.env.GITHUB_KEY;
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
